dbceb399b607384222d4644f1368b478141820f0
LizHuang <search.psop@gmail.com>
2021-09-11 10:38:59 +0800

    avoid no msg to add

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 Vue.createApp({
     data() {
         return {
@@ -9,24 +8,28 @@ Vue.createApp({
     },
     methods: {
         add() {
-        this.todoList.push({
-            id: this.todoList.length + 1,
-            text: this.todo,
-            completed: false, 
+            // avoid no text adding
+            if(this.todo === '') {
+                return
+            }
+            this.todoList.push({
+                id: this.todoList.length + 1,
+                text: this.todo,
+                completed: false, 
         })
-        this.todo = ''
-        this.complete()
-        this.updateLS()
+            this.todo = ''
+            this.complete()
+            this.updateLS()
         },
         updateLS() {
-        localStorage.setItem("todos", JSON.stringify(this.todoList))
+            localStorage.setItem("todos", JSON.stringify(this.todoList))
         },
         remove(todo) {
-        // console.log(todo)
-        const index = this.todoList.findIndex(item => item.id === todo.id)
-        // console.log(index)
-        this.todoList.splice(index, 1)
-        this.updateLS()
+            // console.log(todo)
+            const index = this.todoList.findIndex(item => item.id === todo.id)
+            // console.log(index)
+            this.todoList.splice(index, 1)
+            this.updateLS()
         },
         updateStatus() {
             todo.completed = !todo.completed
@@ -41,14 +44,14 @@ Vue.createApp({
     },
     computed: {
         filterTodo() {
-        switch (this.status) {
-            case 'active': 
-            return this.todoList.filter((todo) => !todo.completed)
-            case 'completed':
-            return this.todoList.filter((todo) => todo.completed)
-            default:
-            return this.todoList
-        }
+            switch (this.status) {
+                case 'active': 
+                return this.todoList.filter((todo) => !todo.completed)
+                case 'completed':
+                return this.todoList.filter((todo) => todo.completed)
+                default:
+                return this.todoList
+            }
         }
     },
     mounted() {
