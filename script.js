5d5e33f46b35f7ea8c9333f89d693d87026714e0
LizHuang <search.psop@gmail.com>
2021-09-16 09:43:15 +0800

    fix filter status doesn't work

--- script.js
+++ script.js
@@ -7,23 +7,28 @@ Vue.createApp({
         }
     },
     methods: {
+        // function: add todo
         add() {
             // avoid no text adding
             if(this.todo === '') {
                 return
             }
+            // add new todo
             this.todoList.push({
                 id: this.todoList.length + 1,
                 text: this.todo,
                 completed: false, 
         })
+            // after add new todo, then clear input
             this.todo = ''
-            // this.complete()
+            // saved in localStorage
             this.updateLS()
         },
+        // function: saved in localStorage
         updateLS() {
             localStorage.setItem("todos", JSON.stringify(this.todoList))
         },
+        // function: remove todo
         remove(todo) {
             // console.log(todo)
             const index = this.todoList.findIndex(item => item.id === todo.id)
@@ -31,24 +36,27 @@ Vue.createApp({
             this.todoList.splice(index, 1)
             this.updateLS()
         },
-        changeStatus() {
-            this.todo.completed = !todo.completed
-        },
-        updateStatus() {
-            todo.completed = !todo.completed
-            this.updateLS()
-        },
+        // changeStatus() {
+        //     this.todo.completed = !todo.completed
+        // },
+        // updateStatus() {
+        //     todo.completed = !todo.completed
+        //     this.updateLS()
+        // },
+        
+        // function: clear all todos
         clear() {
         if (window.confirm('確定要全部刪除？')) {
             this.todoList = []
         }
+        // saved in localStorage
         this.updateLS()
         } 
     },
     computed: {
         filterTodo() {
             switch (this.status) {
-                case 'needTodo': 
+                case 'active': 
                     return this.todoList.filter((todo) => !todo.completed)
                 case 'completed':
                     return this.todoList.filter((todo) => todo.completed)
